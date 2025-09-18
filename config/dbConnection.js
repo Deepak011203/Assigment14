66c3f7e245772deeb01a5d32ed0edc41668a4647
Krishna161003 <132978171+Krishna161003@users.noreply.github.com>
2025-09-18 22:14:53 +0530

    second commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function dbConnection(){
     try{
-        await mongoose.connect("mongodb+srv://rameshmuthu01996:ramesh1996@tasks.tyecyah.mongodb.net/?retryWrites=true&w=majority&appName=tasks" ,{
+        await mongoose.connect("mongodb+srv://deepak01122003abc_db_user:Deepak1973@deepak.hyuy3bo.mongodb.net/?retryWrites=true&w=majority&appName=Deepak" ,{
             ssl:true,
             tlsAllowInvalidCertificates:false
         });
